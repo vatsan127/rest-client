a259f57c4918649bf8ba1c1c33556bd5aa424bb7
Srivatsan <110617159+vatsan127@users.noreply.github.com>
2025-06-09 16:59:35 +0530

    first commit

---
+++ src/main/java/dev/srivatsan/rest_client/RestClientApplication.java
@@ -0,0 +1,13 @@
+package dev.srivatsan.rest_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientApplication.class, args);
+	}
+
+}
