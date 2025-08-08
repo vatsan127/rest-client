d22ba173783269ea8130b8d2c2274debe3806b0a
vatsan127 <vatsan127@gmail.com>
2025-08-08 12:31:02 +0530

    commit for updating restclient with timeout

--- src/main/java/dev/srivatsan/rest_client/config/RestClientConfiguration.java
+++ src/main/java/dev/srivatsan/rest_client/config/RestClientConfiguration.java
@@ -2,13 +2,24 @@ package dev.srivatsan.rest_client.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfiguration {
 
     @Bean
+    @Primary
     public RestClient getRestClientBean() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(5000);
+        requestFactory.setReadTimeout(5000);
+        return RestClient.create();
+    }
+
+    @Bean("deploymentRestClient")
+    public RestClient getRestClientBeanWithoutTimeout() {
         return RestClient.create();
     }
 
--- src/main/java/dev/srivatsan/rest_client/service/RestService.java
+++ src/main/java/dev/srivatsan/rest_client/service/RestService.java
@@ -1,6 +1,7 @@
 package dev.srivatsan.rest_client.service;
 
 import dev.srivatsan.rest_client.model.ToDo;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -14,14 +15,18 @@ public class RestService {
     private final String JSON_PLACEHOLDER_URL = "http://jsonplaceholder.typicode.com/todos/";
 
     private final RestClient restClient;
+    private final RestClient deploymentRestClient;
 
-    public RestService(RestClient restClient) {
+    public RestService(RestClient restClient, @Qualifier("deploymentRestClient") RestClient deploymentRestClient) {
         this.restClient = restClient;
+        this.deploymentRestClient = deploymentRestClient;
     }
 
     public ToDo getToDoByID(int id) {
         String url = JSON_PLACEHOLDER_URL + "/" + id;
-        return restClient
+        RestClient localRestClient = true ? deploymentRestClient : restClient;
+
+        return localRestClient
                 .get()
                 .uri(url)
                 .accept(MediaType.APPLICATION_JSON)
